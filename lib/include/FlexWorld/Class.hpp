0adddea617c849c338df00791e4555830af8df17
Stefan Schindler <stefan@boxbox.org>
2011-12-19 19:24:40 +0100

    Class with adjusted internal data accessors, unit test updated.

--- lib/include/FlexWorld/Class.hpp
+++ lib/include/FlexWorld/Class.hpp
@@ -45,15 +45,21 @@ class Class {
 		const sf::Vector3f& get_origin() const;
 
 		/** Add texture.
-		 * @param texture Texture.
+		 * @param texture Texture (copied).
 		 */
 		void add_texture( const Resource& texture );
 
 		/** Get texture.
 		 * @param index Index.
-		 * @return Resource or null if not found.
+		 * @return Texture resource.
+		 * @throws std::invalid_argument if index is invalid.
 		 */
-		const Resource* get_texture( std::size_t index ) const;
+		const Resource& get_texture( std::size_t index ) const;
+
+		/** Get number of textures.
+		 * @return Number of textures.
+		 */
+		std::size_t get_num_textures() const;
 
 		/** Set hook.
 		 * @param id ID.
@@ -63,20 +69,25 @@ class Class {
 
 		/** Get hook.
 		 * @param id ID.
-		 * @return Hook position or null if hook not found.
+		 * @return Hook.
+		 * @throws std::invalid_argument if ID is invalid.
 		 */
-		const sf::Vector3f* get_hook( const std::string& id ) const;
+		const sf::Vector3f& get_hook( const std::string& id ) const;
+
+		/** Get number of hooks.
+		 * @return Number of hooks.
+		 */
+		std::size_t get_num_hooks() const;
 
 	private:
 		typedef std::map<const std::string, sf::Vector3f> HookMap;
 		typedef std::vector<Resource> ResourceVector;
 
 		ResourceId m_id;
-		std::string m_name;
-		sf::Vector3f m_origin;
-
 		HookMap m_hooks;
 		ResourceVector m_textures;
+		sf::Vector3f m_origin;
+		std::string m_name;
 };
 
 }
