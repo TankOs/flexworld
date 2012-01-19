1231c8a63c226a7729e75e658db9e58584e8834a
Stefan Schindler <stefan@boxbox.org>
2012-01-19 13:05:17 +0100

    LockFacility completed for account manager + unit test.

--- lib/include/FlexWorld/LockFacility.hpp
+++ lib/include/FlexWorld/LockFacility.hpp
@@ -1,24 +1,16 @@
 #pragma once
 
-#include <boost/thread.hpp>
+#include <FlexWorld/RefLock.hpp>
 
 namespace flex {
 
 /** Lock facility for securing several backend objects.
  *
- * The lock facility implements an usage count strategy per thread. That means
- * if the same thread (ID) is requesting a lock (again), the mutex usage
- * counter is increased but not locked again to prevent deadlocks.
- *
- * As soon as the usage counter reaches zero (by unlocking) the mutex is
- * unlocked and other requesters can lock it.
+ * Uses RefLock objects internally, i.e. it's safe to do multiple locks from
+ * the same thread. Do not forget to unlock! ;-)
  */
 class LockFacility {
 	public:
-		/** Ctor.
-		 */
-		LockFacility();
-
 		/** Lock or unlock account manager.
 		 * @param do_lock true to lock, false to unlock.
 		 */
@@ -30,16 +22,7 @@ class LockFacility {
 		bool is_account_manager_locked() const;
 
 	private:
-		struct LockInfo {
-			LockInfo();
-
-			mutable boost::mutex access_mutex;
-			boost::mutex lock_mutex;
-			boost::thread::id last_thread;
-			std::size_t usage_count;
-		};
-
-		LockInfo m_account_manager_lock;
+		RefLock m_account_manager_lock;
 };
 
 }
--- lib/src/FlexWorld/LockFacility.cpp
+++ lib/src/FlexWorld/LockFacility.cpp
@@ -2,65 +2,17 @@
 
 namespace flex {
 
-LockFacility::LockInfo::LockInfo() :
-	usage_count( 0 )
-{
-}
-
-LockFacility::LockFacility() {
-}
-
 void LockFacility::lock_account_manager( bool do_lock ) {
 	if( do_lock ) {
-		{
-			boost::lock_guard<boost::mutex> guard( m_account_manager_lock.access_mutex );
-
-			// At first check if the same thread is trying to lock again.
-			if( m_account_manager_lock.last_thread == boost::this_thread::get_id() ) {
-				++m_account_manager_lock.usage_count;
-				return;
-			}
-		}
-
-		// At this point the lock has been acquired by another thread. Wait for it to
-		// become available.
-		m_account_manager_lock.lock_mutex.lock();
-
-		// Store info.
-		{
-			boost::lock_guard<boost::mutex> guard( m_account_manager_lock.access_mutex );
-			
-			++m_account_manager_lock.usage_count;
-			m_account_manager_lock.last_thread = boost::this_thread::get_id();
-		}
+		m_account_manager_lock.lock();
 	}
 	else {
-		{
-			boost::lock_guard<boost::mutex> guard( m_account_manager_lock.access_mutex );
-
-			assert( m_account_manager_lock.last_thread == boost::this_thread::get_id() );
-			assert( m_account_manager_lock.usage_count > 0 );
-
-			--m_account_manager_lock.usage_count;
-
-			// If not more in use, release.
-			if( m_account_manager_lock.usage_count == 0 ) {
-				m_account_manager_lock.last_thread = boost::thread::id();
-				m_account_manager_lock.lock_mutex.unlock();
-			}
-		}
+		m_account_manager_lock.unlock();
 	}
 }
 
 bool LockFacility::is_account_manager_locked() const {
-	bool is_locked = false;
-
-	{
-		boost::lock_guard<boost::mutex> lock( m_account_manager_lock.access_mutex );
-		is_locked = (m_account_manager_lock.usage_count > 0);
-	}
-
-	return is_locked;
+	return m_account_manager_lock.get_usage_count() > 0;
 }
 
 }
