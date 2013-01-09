89840f04a748f1c2578c19db39f651e38fb8f879
Stefan Schindler <stefan@boxbox.org>
2013-01-09 02:05:40 +0100

    X11 threads fix.

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -1,6 +1,15 @@
 #include "Client.hpp"
 
+#ifdef LINUX
+#include <X11/Xlib.h>
+#endif
+
 int main() {
+#ifdef LINUX
+	// Fix SFML multithreading bug.
+	XInitThreads();
+#endif
+
 	Client app;
 
 	app.run();
